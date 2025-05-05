fc0053610922dda794d6b560a99523e173be373e
DaviQuaresma <quaresma.davi@gmail.com>
2025-05-05 17:21:51 -0300

    Correção da inserção no banco de dados

--- src/utils/dbCommands.js
+++ src/utils/dbCommands.js
@@ -1,52 +1,111 @@
 /** @format */
 
-async function insertSale(connection, total, arredonda, desconto) {
-	const ta_codigo = 7; // da tabela_preco
-	const to_codigo = 1; // da tipo_operacao
-	const tx_codigo = 1; // da terminal_caixa
-	const ve_codigo = 1; // da vendedor
-	const est_codigo = 1; // da estado_pedido
-	const cli_codigo = 2;
-	const par_cp_codigo = 2;
-	const emp_codigo = 1;
-	const usu_codigo = 1;
-	const ven_tab_preco = 2;
-	const ven_modelo_venda = 1;
-
-	const { rows } = await connection.query(
-		`INSERT INTO vendas (
-			ven_data, ven_total, ven_arredonda, ven_desconto,
-			cli_codigo, par_cp_codigo, est_codigo, ve_codigo,
-			ven_status, ven_tipo, ven_tab_preco, to_codigo,
-			ta_codigo, tx_codigo, emp_codigo, usu_codigo,
-			ven_modelo_venda, ven_tipo_pag_jor, ven_data_hora_finaliza,
-			ven_hora
-		) VALUES (
-			NOW(), $1, $2, $3,
-			$4, $5, $6, $7,
-			'V', 'P', $8, $9,
-			$10, $11, $12, $13,
-			$14, 'P', NOW(),
-			TO_CHAR(NOW(), 'HH24:MI:SS')
-		) RETURNING ven_cod_pedido`,
-		[
-			total,
-			arredonda,
-			desconto,
+async function insertSale(
+	connection,
+	total,
+	arredonda,
+	desconto,
+	usu_codigo = -161
+) {
+	const insertQuery = `
+		INSERT INTO vendas (
+			ven_data,
+			ven_desconto,
 			cli_codigo,
 			par_cp_codigo,
 			est_codigo,
+			ven_total,
+			ven_arredonda,
+			ven_data_hora_finaliza,
+			ven_hora,
 			ve_codigo,
+			ven_tipo,
 			ven_tab_preco,
+			ven_status,
 			to_codigo,
 			ta_codigo,
-			tx_codigo,
-			emp_codigo,
+			ven_impressao,
+			ven_tipo_percentual,
+			ven_porc_fatura,
+			ven_porc_nota,
+			ven_cupom,
+			finalizado,
+			checked,
+			nf_numero,
 			usu_codigo,
+			ven_troco,
 			ven_modelo_venda,
-		]
-	);
+			ven_data_hora_faturamento,
+			ven_obs_interna,
+			ven_modelo_dfe,
+			ven_numero_dfe,
+			ven_dfe,
+			ven_modelo_nfe,
+			ven_status_carregamento,
+			ven_frete,
+			ven_ipi,
+			ven_icms_st,
+			ven_mov_estoque,
+			ven_tipo_pag_jor,
+			emp_codigo,
+			ven_tipo_frete,
+			ven_aplicacao,
+			ven_obs
+		) VALUES (
+			NOW(),               -- $1  ven_data
+			$1,                  -- $2  ven_desconto
+			1,                   -- $3  cli_codigo
+			2,                   -- $4  par_cp_codigo
+			14,                  -- $5  est_codigo
+			$2,                  -- $6  ven_total
+			$3,                  -- $7  ven_arredonda
+			NOW(),               -- $8  ven_data_hora_finaliza
+			TO_CHAR(NOW(), 'HH24:MI:SS'), -- $9  ven_hora
+			1,                   -- $10 ve_codigo
+			'V',                 -- $11 ven_tipo
+			2,                   -- $12 ven_tab_preco
+			'Y',                 -- $13 ven_status
+			1,                   -- $15 to_codigo
+			7,                   -- $16 ta_codigo
+			NOW(),                 -- $17 ven_impressao
+			1,                 -- $18 ven_tipo_percentual
+			1,                   -- $19 ven_porc_fatura
+			1,                   -- $20 ven_porc_nota
+			'S',                 -- $21 ven_cupom
+			'N',                 -- $22 finalizado
+			'N',                 -- $23 checked
+			1,                   -- $24 nf_numero
+			$4,                  -- $25 usu_codigo
+			0.0,                 -- $26 ven_troco
+			1,                   -- $27 ven_modelo_venda
+			NOW(),               -- $28 ven_data_hora_faturamento
+			'VENDA FRENTE DE CAIXA 1', -- $29 ven_obs_interna
+			'65',                -- $30 ven_modelo_dfe
+			1,                   -- $31 ven_numero_dfe
+			1,                   -- $32 ven_dfe
+			'65',                -- $33 ven_modelo_nfe
+			'N',                 -- $34 ven_status_carregamento
+			0.0,                 -- $35 ven_frete
+			0.0,                 -- $36 ven_ipi
+			0.0,                 -- $37 ven_icms_st
+			'P',                 -- $38 ven_mov_estoque
+			'P',                 -- $39 ven_tipo_pag_jor
+			1,                   -- $40 emp_codigo
+			1,                 -- $41 ven_tipo_frete
+			'A',                 -- $42 ven_aplicacao
+			'VENDA FRENTE DE CAIXA 1'  -- $43 ven_obs
+		)
+		RETURNING ven_cod_pedido;
+	`;
 
+	const values = [
+		desconto, // $1
+		total, // $2
+		arredonda, // $3
+		usu_codigo, // $4
+	];
+
+	const { rows } = await connection.query(insertQuery, values);
 	return rows[0].ven_cod_pedido;
 }
 
